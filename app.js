7d4c2ea88f407e0cb8086a786ae94b25e638bae7
Tom Henike <tomhenike@gmail.com>
2023-10-09 17:49:23 +0200

    feat: new files and app changes

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const _ = require("lodash");
+const { readFile } = require("fs");
+const path = require("path");
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+getText("./content/first.txt")
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
+
+const start = async () => {
+  const first = await getText("./content/first.txt");
+};
